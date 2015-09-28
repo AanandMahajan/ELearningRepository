fcfb459789b125d7182c94575487144936abf6db
Anand Mahajan <anand.mahajan@hotmail.com>
2015-09-28 17:51:44 +0530

    Test Change

--- ELearningWebApp/Global.asax.cs
+++ ELearningWebApp/Global.asax.cs
@@ -22,6 +22,7 @@ namespace ELearning.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //AuthConfig.RegisterAuth();
             AuthConfig.RegisterAuth();
         }
     }
