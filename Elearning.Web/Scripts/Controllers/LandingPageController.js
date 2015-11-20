985c736001f6e4238ebffa99ae0fb03237c12287
Onkar <omkarpatil48@gmail.com>
2015-11-20 18:54:01 +0530

    Adding Elearning Angular Web App without MVC

---
+++ Elearning.Web/Scripts/Controllers/LandingPageController.js
@@ -0,0 +1,114 @@
+﻿
+var LandingPageController = function ($scope, $location, $http, WebAPIBaseURL) {
+    //$scope.authentication = false;
+    $scope.user = '';
+    $scope.publisher = {};    
+
+    //$scope.WebApiBaseURL = 'http://pad22991/';
+
+    $location.path('/routeSearch');
+
+    $scope.login = function (myModal) {
+        //var result = LoginFactory($scope.loginForm.emailAddress, $scope.loginForm.password, $scope.loginForm.rememberMe);
+
+        //result.then(function (result) {
+        //    if (result.success) {
+        //        if ($scope.loginForm.returnUrl !== undefined) {
+        //            $location.path('/routeSearch');
+        //        } else {
+        //            $location.path($scope.loginForm.returnUrl);
+        //        }
+        //    } else {
+        //        $scope.loginForm.loginFailure = true;
+        //    }
+        //});
+
+        var user_new = {}                    
+        user_new.Password = $scope.password;
+        user_new.Email = $scope.user;
+
+        var test = angular.toJson(user_new);          
+                 
+        $http.get('http://172.25.217.78/api/User/5', { headers: { 'Content-Type': 'application/json' } }).then(
+
+            function successCallback(res)
+            {
+                console.log(res);
+            },
+            function errorCallback(res)
+            {
+                console.log(res);
+            }
+        );             
+
+        
+
+        //Validate User
+       // $http.post('http://localhost:49923/api/User/', angular.toJson(user_new)).then(
+
+       //    function successCallback(res) {
+       //        console.log(res);
+       //    },
+       //    function errorCallback(res) {
+       //        console.log(res);
+       //    }
+       //);
+       
+        //$scope.authentication = true;
+        //console.log("Username: " + $scope.user + " pwd:" + $scope.password);
+        //$location.path('/routeHome/' + $scope.user + '/' + $scope.password);        
+    };
+
+
+    
+    $scope.logout = function () {
+        console.log("loging out..");
+        $scope.authentication = false;
+        $location.path('/routeSearch');
+    };
+
+    $scope.AddUser = function (myModal) {
+
+        //console.log("Full name " + $scope.publisher.fullname);
+        //console.log("Email " + $scope.publisher.email);
+
+        //$http.post('http://172.25.217.78/api/User/', angular.toJson($scope.publisher)).then(
+
+        //    function successCallback(res) {
+        //        console.log(res);
+        //    },
+        //    function errorCallback(res) {
+        //        console.log(res);
+        //    }
+        //);
+        
+        //$http.defaults.headers.common = {};
+        //$http.defaults.headers.post = {};
+        //$http.defaults.headers.put = {};
+        //$http.defaults.headers.patch = {};
+        
+        $http.post('http://172.25.217.78/api/user/onkar/1', angular.toJson($scope.publisher)).then(
+
+            function successCallback(res) {
+                console.log(res);
+            },
+            function errorCallback(res) {
+                console.log(res);
+            }
+        );
+
+      //  $http.post('http://localhost:49923/api/User/', angular.toJson($scope.publisher)).then(
+
+      //    function successCallback(res) {
+      //        console.log(res);
+      //    },
+      //    function errorCallback(res) {
+      //        console.log(res);
+      //    }
+      //);
+
+    };    
+}
+
+// The inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
+LandingPageController.$inject = ['$scope', '$location', '$http', WebAPIBaseURL];
---
+++ Elearning.Web/Scripts/ELearning.Web.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="../Views/RoutesDemo/Search.html" />
+/// <reference path="../Views/RoutesDemo/Search.html" />
+var ELearningWeb = angular.module('ELearning.Web', ['ngRoute']);
+
+ELearningWeb.constant('WebAPIBaseURL', 'http://pad22991/');
+
+ELearningWeb.controller('LandingPageController', LandingPageController);
+ELearningWeb.controller('LoginController', LoginController);
+ELearningWeb.controller('RegisterController', RegisterController);
+
+ELearningWeb.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+ELearningWeb.factory('LoginFactory', LoginFactory);
+ELearningWeb.factory('RegistrationFactory', RegistrationFactory);
+
+var configFunction = function ($routeProvider, $httpProvider) {
+    $routeProvider.
+        when('/routeSearch', {
+           // templateUrl: 'routesDemo/Search'
+            controller: SearchController,
+            templateUrl: '../RoutesDemo/Search.html'
+        })
+        //.when('/routeTwo/:donuts', {
+        //    templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+        //})
+        //.when('/routeHome', {
+        //    templateUrl: 'routesDemo/Home'
+        //})
+         .when('/routeHome/:username/:password', {
+             templateUrl: function (params) { return '/routesDemo/Home?username=' + params.username + '&password=' + params.password; }
+         })
+        .when('/routeCourseDetails', {
+            templateUrl: 'routesDemo/CourseDetails'
+        })
+        .when('/login', {
+            templateUrl: '/Account/Login',
+            controller: LoginController
+        })
+        .when('/register', {
+            templateUrl: '/Account/Register',
+            controller: RegisterController
+        });
+
+    $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+}
+configFunction.$inject = ['$routeProvider', '$httpProvider'];
+
+ELearningWeb.config(configFunction);
