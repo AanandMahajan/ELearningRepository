3d8a9d56db08982c298b602553062213a530a559
meetprashantsalvi <meetprashantsalvi@gmail.com>
2015-12-01 15:37:42 +0530

    Course details Expander

--- Elearning.Web/Scripts/Controllers/CourseDetailsController.js
+++ Elearning.Web/Scripts/Controllers/CourseDetailsController.js
@@ -35,6 +35,25 @@
         $scope.isDescription = true;
     }
 
+    function close_accordion_section(indexer) {
+        $('.accordion ' + '#accordionParent-' + indexer).removeClass('active');
+        $('.accordion ' + '#accordionChild-' + indexer).slideUp(300).removeClass('open');
+    }
+
+    $scope.clickSectionTitle = function (indexer) {
+        // Grab current anchor value
+        var accordionParent = "#accordionParent-" + indexer;
+
+        if ($(accordionParent).is('.active')) {
+            close_accordion_section(indexer);
+        } else {
+            close_accordion_section(indexer);
+            // Add active class to section title
+            $(accordionParent).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + '#accordionChild-' + indexer).slideDown(300).addClass('open');
+        }
+    }
 }
 
 // The inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
