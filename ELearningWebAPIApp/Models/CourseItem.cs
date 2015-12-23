31c8cd844f60c3634ceb69f1e2467be701b5f72c
Harshal <harshal_bawaskar@live.in>
2015-12-23 12:25:40 +0530

    Added images in course carousel on UserHome page.

---
+++ ELearningWebAPIApp/Models/CourseItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ELearning.WebAPI.DBModel;
+
+namespace ELearning.WebAPI.Models
+{
+    public class CourseItem
+    {
+        private string BlobUrl;
+        private CourseDetail Course;
+
+        public string BlobURL
+        {
+            set
+            {
+                BlobUrl = value;
+            }
+            get
+            {
+                return BlobUrl;
+            }
+        }
+        public CourseDetail CourseDetailItem
+        {
+            set
+            {
+                Course = value;
+            }
+            get
+            {
+                return Course;
+            }
+
+        }
+
+        public CourseItem() { }
+        public CourseItem(string BlobUrl, CourseDetail Course)
+        {
+            this.BlobUrl = BlobUrl;
+            this.Course = Course;
+        }
+    }
+}
